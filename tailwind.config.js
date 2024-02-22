2d0dbea0dea262b2db6689fe6791f10c1d3ea4c7
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2024-02-22 20:00:23 +0100

    Started Working on Nav

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "grey": "#f8f8f8",
+        "grey-2": "#929292"
+      }
+    },
+  },
+  plugins: [],
+}
+
