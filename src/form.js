31fb31ba0392014f557591fe381943f5c34102bf
feyiamujo1 <feyisayoamujoyegbe@gmail.com>
2024-03-11 11:06:58 +0100

    Updated the form functionality

--- src/form.js
+++ src/form.js
@@ -13,21 +13,24 @@ const error_response_indicator = document.getElementById("error_response");
 const SubmitContactForm = e => {
   e.preventDefault();
   contactFormButton.disabled = true;
+  const access_key = "a502968f-9479-4c40-8998-a0a38cbc88a6";
+  const formData = {
+    access_key: access_key,
+    Name: fullname.value,
+    Email: email.value,
+    Phone: phoneNumber.value,
+    Subject: subject.value,
+    Message: message.value
+  };
+  const json = JSON.stringify(formData);
+
   buttonText.classList.add("hidden");
   buttonLoader.classList.remove("hidden");
   error_response_indicator.classList.add("hidden");
   correct_response_indicator.classList.add("hidden");
   axios.defaults.headers.post["Content-Type"] = "application/json";
   axios
-    .post("https://formsubmit.co/umagineagency@gmail.com", {
-      _subject: "Contact Form Questions/Inquiries",
-      _template: "box",
-      Name: fullname.value,
-      Email: email.value,
-      Phone: phoneNumber.value,
-      Subject: subject.value,
-      Message: message.value
-    })
+    .post("https://api.web3forms.com/submit", json)
     .then(response => {
       if (response.status === 200) {
         correct_response_indicator.classList.remove("hidden");
